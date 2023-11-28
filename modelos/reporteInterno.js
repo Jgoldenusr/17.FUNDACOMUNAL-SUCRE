e94870f88e5e8c53235008b8c57d27cbf4c67bf8
Jgoldenusr <jesusdgonzalez.ve@gmail.com>
2023-11-28 06:32:01 -0400

    actualizado esquema de reporte interno

--- modelos/reporteInterno.js
+++ modelos/reporteInterno.js
@@ -4,11 +4,7 @@ const ReporteBase = require("./reporte");
 const Esquema = mongoose.Schema;
 
 const ReporteInterno = new Esquema({
-  condicionVoceria: { type: String, required: true },
-  estaEnSistema: { type: Boolean, default: false },
-  estaRenovado: { type: Boolean, default: false },
-  estaRegistrado: { type: Boolean, default: false },
-  fechaVencimiento: { type: Date, default: Date.now() },
+  fechaRegistro: { type: Date, default: Date.now() },
 });
 
 module.exports = ReporteBase.discriminator("interno", ReporteInterno);
