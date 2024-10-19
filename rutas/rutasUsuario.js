49fed2da66f480d62674641bf1e335164db96309
Jgoldenusr <jesusdgonzalez.ve@gmail.com>
2024-10-19 09:03:37 -0400

    redireccion de acceso denegado y autorizacion de autogestion

--- rutas/rutasUsuario.js
+++ rutas/rutasUsuario.js
@@ -3,6 +3,8 @@ const enrutador = express.Router();
 const controladorUsuario = require("../controladores/controladorUsuario");
 const permisos = require("../config/permisos");
 
+enrutador.get("/denegado", controladorUsuario.denegado);
+
 enrutador.post("/ingresar", controladorUsuario.iniciarSesion);
 
 enrutador.use(permisos.autenticarToken);
@@ -11,13 +13,15 @@ enrutador.get("/cuenta", controladorUsuario.miCuenta);
 
 enrutador.use(permisos.autorizarRol);
 
-enrutador.delete("/:id", controladorUsuario.borrarUsuario);
-
 enrutador.get("/", controladorUsuario.listarUsuarios);
 
 enrutador.get("/:id", controladorUsuario.buscarUsuario);
 
 enrutador.post("/", controladorUsuario.registrarUsuario);
+
+enrutador.use(permisos.autorizarAutogestion);
+
+enrutador.delete("/:id", controladorUsuario.borrarUsuario);
 
 enrutador.put("/:id", controladorUsuario.actualizarUsuario);
 
