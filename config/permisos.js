4b674d039e9ab0987f7b420a74ec68e722d8dd3a
Jgoldenusr <jesusdgonzalez.ve@gmail.com>
2024-10-19 09:01:25 -0400

    un admin no puede actualizar ni borrar su cuenta, redireccion al fallar auth

--- config/permisos.js
+++ config/permisos.js
@@ -12,6 +12,45 @@ const listaNegraPromotor = [
 
 exports.autenticarToken = passport.authenticate("jwt", {
   session: false,
+  failureRedirect: "/usuarios/denegado",
+});
+
+exports.autorizarAutogestion = function (req, res, next) {
+  const partesURL = req.path.split("/"); //Dividimos la url segun los slashs
+  const idUsuario = partesURL[partesURL.length - 1]; //Buscamos lo ultimo de la url, que es la id del usuario
+
+  if (idUsuario === req.user._id.toString()) {
+    return res.status(401).json({
+      error: {
+        message:
+          "Accion denegada, contacte con otro administrador para llevarla a cabo",
+      },
+    });
+  } else {
+    return next();
+  }
+};
+
+exports.autorizarCambio = asyncHandler(async function (req, res, next) {
+  if (req.user.rol === "ADMINISTRADOR") {
+    return next();
+  } else {
+    const partesURL = req.path.split("/"); //Dividimos la url segun los slashs
+    const idReporte = partesURL[partesURL.length - 1]; //Buscamos lo ultimo de la url, que es la id del reporte
+    //Hacemos la consulta
+    const reporteAsociado = await Reporte.findOne({
+      _id: idReporte,
+      usuario: req.user._id,
+    }).exec();
+    //Si tuvo exito, pasamos al siguiente middleware, sino, desautorizamos
+    return reporteAsociado
+      ? next()
+      : res.status(401).json({
+          error: {
+            message: "Usuario no asociado al reporte",
+          },
+        });
+  }
 });
 
 exports.autorizarRol = function (req, res, next) {
@@ -36,25 +75,3 @@ exports.autorizarRol = function (req, res, next) {
         },
       });
 };
-
-exports.autorizarCambio = asyncHandler(async function (req, res, next) {
-  if (req.user.rol === "ADMINISTRADOR") {
-    return next();
-  } else {
-    const partesURL = req.path.split("/"); //Dividimos la url segun los slashs
-    const idReporte = partesURL[partesURL.length - 1]; //Buscamos lo ultimo de la url, que es la id del reporte
-    //Hacemos la consulta
-    const reporteAsociado = await Reporte.findOne({
-      _id: idReporte,
-      usuario: req.user._id,
-    }).exec();
-    //Si tuvo exito, pasamos al siguiente middleware, sino, desautorizamos
-    return reporteAsociado
-      ? next()
-      : res.status(401).json({
-          error: {
-            message: "Usuario no asociado al reporte",
-          },
-        });
-  }
-});
