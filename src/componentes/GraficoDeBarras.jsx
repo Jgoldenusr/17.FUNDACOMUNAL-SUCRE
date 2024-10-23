0e16e4fd1fb12e040cea05cfc5108647acced93d
Jgoldenusr <jesusdgonzalez.ve@gmail.com>
2024-10-23 16:47:09 -0400

    agregada informacion sobre el grafico

--- src/componentes/GraficoDeBarras.jsx
+++ src/componentes/GraficoDeBarras.jsx
@@ -1,7 +1,17 @@
 import { ResponsiveBar } from "@nivo/bar";
 import { useNavigate } from "react-router-dom";
+//Componentes MUI
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@mui/material";
+//Iconos MUI
+import AnalyticsRoundedIcon from "@mui/icons-material/AnalyticsRounded";
 
-function GraficoDeBarras({ data, colores }) {
+function GraficoDeBarras({ data, colores, titulo }) {
   const navegarHasta = useNavigate();
 
   const esDispositivoMovil = window.innerWidth < 767;
@@ -41,82 +51,100 @@ function GraficoDeBarras({ data, colores }) {
   };
   /* jshint ignore:start */
   return (
-    <div id="BC-GENERAL" style={{ height: "100%" }}>
-      <ResponsiveBar
-        colors={colores}
-        data={data}
-        keys={["Renovados", "No renovados", "Vigentes", "No vigentes"]}
-        indexBy="municipio"
-        groupMode="stacked"
-        layout="horizontal"
-        margin={{
-          top: 10,
-          right: esDispositivoMovil ? 10 : 80,
-          bottom: esDispositivoMovil ? 110 : 80,
-          left: 100,
-        }}
-        labelSkipWidth={12}
-        labelSkipHeight={12}
-        enableGridX={true}
-        enableGridY={true}
-        axisBottom={esDispositivoMovil ? null : {}}
-        axisLeft={{
-          renderTick: truncarTexto,
-        }}
-        onMouseEnter={(e) => {
-          document.querySelector("#BC-GENERAL").style.cursor = "pointer";
-        }}
-        onMouseLeave={(e) => {
-          document.querySelector("#BC-GENERAL").style.cursor = "default";
-        }}
-        onClick={(n, e) => {
-          let url = `/ccs?municipios=${n.indexValue}&`;
-          switch (n.id) {
-            case "No renovados": {
-              url += "estatus=norenovado";
-              break;
-            }
-            case "No vigentes": {
-              url += "estatus=novigente";
-              break;
-            }
-            case "Renovados": {
-              url += "estatus=renovado";
-              break;
-            }
-            case "Vigentes": {
-              url += "estatus=vigente";
-              break;
-            }
-          }
-          navegarHasta(url);
-        }}
-        legends={[
-          {
-            anchor: esDispositivoMovil ? "bottom-left" : "bottom",
-            direction: esDispositivoMovil ? "column" : "row",
-            translateX: 0,
-            translateY: esDispositivoMovil ? 100 : 55,
-            itemsSpacing: 15,
-            itemWidth: 100,
-            itemHeight: 10,
-            itemTextColor: "#999",
-            itemDirection: "left-to-right",
-            itemOpacity: 1,
-            symbolSize: 20,
-            symbolShape: "circle",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemTextColor: "#000",
-                },
-              },
-            ],
-          },
-        ]}
+    <Card elevation={3}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "#1976d2" }}>
+            <AnalyticsRoundedIcon />
+          </Avatar>
+        }
+        disableTypography
+        title={
+          <Typography component="div" variant="subtitle1">
+            {titulo}
+          </Typography>
+        }
+        sx={{ bgcolor: "#1976d2", color: "white", py: 1 }}
       />
-    </div>
+      <CardContent sx={{ height: { xs: "100vh", md: "150vh" } }}>
+        <div id="BC-GENERAL" style={{ height: "100%" }}>
+          <ResponsiveBar
+            colors={colores}
+            data={data}
+            keys={["Renovados", "No renovados", "Vigentes", "No vigentes"]}
+            indexBy="municipio"
+            groupMode="stacked"
+            layout="horizontal"
+            margin={{
+              top: 10,
+              right: esDispositivoMovil ? 10 : 80,
+              bottom: esDispositivoMovil ? 110 : 80,
+              left: 100,
+            }}
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            enableGridX={true}
+            enableGridY={true}
+            axisBottom={esDispositivoMovil ? null : {}}
+            axisLeft={{
+              renderTick: truncarTexto,
+            }}
+            onMouseEnter={(e) => {
+              document.querySelector("#BC-GENERAL").style.cursor = "pointer";
+            }}
+            onMouseLeave={(e) => {
+              document.querySelector("#BC-GENERAL").style.cursor = "default";
+            }}
+            onClick={(n, e) => {
+              let url = `/ccs?municipios=${n.indexValue}&`;
+              switch (n.id) {
+                case "No renovados": {
+                  url += "estatus=norenovado";
+                  break;
+                }
+                case "No vigentes": {
+                  url += "estatus=novigente";
+                  break;
+                }
+                case "Renovados": {
+                  url += "estatus=renovado";
+                  break;
+                }
+                case "Vigentes": {
+                  url += "estatus=vigente";
+                  break;
+                }
+              }
+              navegarHasta(url);
+            }}
+            legends={[
+              {
+                anchor: esDispositivoMovil ? "bottom-left" : "bottom",
+                direction: esDispositivoMovil ? "column" : "row",
+                translateX: 0,
+                translateY: esDispositivoMovil ? 100 : 55,
+                itemsSpacing: 15,
+                itemWidth: 100,
+                itemHeight: 10,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 20,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+      </CardContent>
+    </Card>
   );
   /* jshint ignore:end */
 }
--- src/componentes/GraficoDePastel.jsx
+++ src/componentes/GraficoDePastel.jsx
@@ -1,77 +1,105 @@
 import { ResponsivePie } from "@nivo/pie";
 import { useNavigate } from "react-router-dom";
+//Componentes MUI
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@mui/material";
+//Iconos MUI
+import DataUsageRoundedIcon from "@mui/icons-material/DataUsageRounded";
 
-function GraficoDePastel({ data, colores, nombre }) {
+function GraficoDePastel({ data, colores, nombre, titulo }) {
   const navegarHasta = useNavigate();
 
   const esDispositivoMovil = window.innerWidth < 767;
   /* jshint ignore:start */
   return (
-    <div id={`PC-${nombre}`} style={{ height: "100%" }}>
-      <ResponsivePie
-        data={data}
-        colors={colores}
-        margin={{ top: 40, right: 40, bottom: 80, left: 40 }}
-        innerRadius={0.5}
-        arcLinkLabelsThickness={2}
-        arcLinkLabelsColor={{ from: "color" }}
-        arcLabelsSkipAngle={10}
-        activeOuterRadiusOffset={10}
-        enableArcLinkLabels={esDispositivoMovil < 767 ? false : true}
-        onMouseEnter={(e) => {
-          document.querySelector(`#PC-${nombre}`).style.cursor = "pointer";
-        }}
-        onMouseLeave={(e) => {
-          document.querySelector(`#PC-${nombre}`).style.cursor = "default";
-        }}
-        onClick={(n, e) => {
-          let url = `/ccs?`;
-          switch (n.id) {
-            case "No renovados": {
-              url += "estatus=norenovado";
-              break;
-            }
-            case "No vigentes": {
-              url += "estatus=novigente";
-              break;
-            }
-            case "Renovados": {
-              url += "estatus=renovado";
-              break;
-            }
-            case "Vigentes": {
-              url += "estatus=vigente";
-              break;
-            }
-          }
-          navegarHasta(url);
-        }}
-        legends={[
-          {
-            anchor: "bottom",
-            direction: "row",
-            translateX: 0,
-            translateY: 55,
-            itemsSpacing: 0,
-            itemWidth: 100,
-            itemHeight: 10,
-            itemTextColor: "#999",
-            itemDirection: "left-to-right",
-            itemOpacity: 1,
-            symbolSize: 20,
-            symbolShape: "circle",
-            effects: [
-              {
-                on: "hover",
-                style: {
-                  itemTextColor: "#000",
-                },
-              },
-            ],
-          },
-        ]}
+    <Card elevation={3}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "#1976d2" }}>
+            <DataUsageRoundedIcon />
+          </Avatar>
+        }
+        disableTypography
+        title={
+          <Typography component="div" variant="subtitle1">
+            {titulo}
+          </Typography>
+        }
+        sx={{ bgcolor: "#1976d2", color: "white", py: 1 }}
       />
-    </div>
+      <CardContent sx={{ height: "45vh" }}>
+        <div id={`PC-${nombre}`} style={{ height: "100%" }}>
+          <ResponsivePie
+            data={data}
+            colors={colores}
+            margin={{ top: 40, right: 40, bottom: 80, left: 40 }}
+            innerRadius={0.5}
+            arcLinkLabelsThickness={2}
+            arcLinkLabelsColor={{ from: "color" }}
+            arcLabelsSkipAngle={10}
+            activeOuterRadiusOffset={10}
+            enableArcLinkLabels={esDispositivoMovil < 767 ? false : true}
+            onMouseEnter={(e) => {
+              document.querySelector(`#PC-${nombre}`).style.cursor = "pointer";
+            }}
+            onMouseLeave={(e) => {
+              document.querySelector(`#PC-${nombre}`).style.cursor = "default";
+            }}
+            onClick={(n, e) => {
+              let url = `/ccs?`;
+              switch (n.id) {
+                case "No renovados": {
+                  url += "estatus=norenovado";
+                  break;
+                }
+                case "No vigentes": {
+                  url += "estatus=novigente";
+                  break;
+                }
+                case "Renovados": {
+                  url += "estatus=renovado";
+                  break;
+                }
+                case "Vigentes": {
+                  url += "estatus=vigente";
+                  break;
+                }
+              }
+              navegarHasta(url);
+            }}
+            legends={[
+              {
+                anchor: "bottom",
+                direction: "row",
+                translateX: 0,
+                translateY: 55,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 10,
+                itemTextColor: "#999",
+                itemDirection: "left-to-right",
+                itemOpacity: 1,
+                symbolSize: 20,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000",
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        </div>
+      </CardContent>
+    </Card>
   );
   /* jshint ignore:end */
 }
